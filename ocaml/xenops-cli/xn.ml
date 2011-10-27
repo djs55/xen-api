d0a3f239104441943cace2b9d0b1b287d7d65273
David Scott <dave.scott@eu.citrix.com>
2011-10-27 21:39:41 +0100

    xn: hook up "create"

--- ocaml/xenops-cli/xn.ml
+++ ocaml/xenops-cli/xn.ml
@@ -18,14 +18,86 @@ let usage () =
 	Printf.fprintf stderr "%s <command> [args] - send commands to the xenops daemon\n" Sys.argv.(0);
 	Printf.fprintf stderr "%s create <config> - create a VM from <config>\n" Sys.argv.(0)
 
+open Xenops_interface
+open Xmlrpc_client
+let default_path = "/var/xapi/xenopsd"
+let transport = ref (Unix default_path)
+
+let rpc call =
+	XMLRPC_protocol.rpc ~transport:!transport
+		~http:(xmlrpc ~version:"1.0" "/") call
+
+let success = function
+	| (_, Some x) -> failwith (Jsonrpc.to_string (rpc_of_error x))
+	| (Some x, _) -> x
+	| None, None -> failwith "protocol error"
+
 let create filename =
-	let (_: bool) = Parsing.set_trace true in
 	Unixext.with_input_channel filename
 		(fun ic ->
 			let lexbuf = Lexing.from_channel ic in
 			let config = Xn_cfg_parser.file Xn_cfg_lexer.token lexbuf in
 			let open Xn_cfg_types in
-			Printf.fprintf stderr "Parsed: %s\n" (config |> rpc_of_config |> Jsonrpc.to_string)
+			Printf.fprintf stderr "Parsed: %s\n" (config |> rpc_of_config |> Jsonrpc.to_string);
+			let mem x = List.mem_assoc x config in
+			let find x = List.assoc x config in
+			let any xs = List.fold_left (||) false (List.map mem xs) in
+			let pv =
+				false
+				|| (mem _builder && (find _builder |> string = "linux"))
+				|| (not(mem _builder) && (any [ _bootloader; _kernel ])) in
+			Printf.fprintf stderr "PV = %b\n" pv;
+			let open Vm in
+			let builder_info = match pv with
+				| true -> PV {
+					boot =
+						if mem _bootloader then Indirect {
+							bootloader = find _bootloader |> string;
+							extra_args = "";
+							legacy_args = "";
+							bootloader_args = "";
+							devices = [];
+						} else if mem _kernel then Direct {
+							kernel = find _kernel |> string;
+							cmdline = if mem _root then find _root |> string else "";
+							ramdisk = if mem _ramdisk then Some (find _ramdisk |> string) else None;
+						} else begin
+							List.iter (Printf.fprintf stderr "%s\n") [
+								"I couldn't determine how to start this VM.";
+								Printf.sprintf "A PV guest needs either %s or %s and %s" _bootloader _kernel _ramdisk
+							];
+							exit 1
+						end
+					}
+				| false -> HVM {
+					hap = true;
+					shadow_multiplier = 1.;
+					timeoffset = "";
+					video_mib = 4;
+				} in
+			let uuid = if mem _uuid then find _uuid |> string else "c0ffeec0-ffee-c0ff-eec0-ffeec0ffeec0" in
+			let name = if mem _name then find _name |> string else uuid in
+			let mib = if mem _memory then find _memory |> int |> Int64.of_int else 64L in
+			let bytes = Int64.mul 1024L (Int64.mul 1024L mib) in
+			let vcpus = if mem _vcpus then find _vcpus |> int else 1 in
+			let vm = {
+				id = uuid;
+				domid = None;
+				name = name;
+				ssidref = 0l;
+				xsdata = [];
+				platformdata = [];
+				bios_strings = [];
+				ty = builder_info;
+				suppress_spurious_page_faults = false;
+				machine_address_size = None;
+				memory_static_max = bytes;
+				memory_dynamic_max = bytes;
+				memory_dynamic_min = bytes;
+				vcpus = vcpus
+			} in
+			let (id: Vm.id) = success (Client.VM.create rpc vm) in
+			Printf.printf "%s\n" id
 		)
 
 let _ =
--- ocaml/xenops-cli/xn_cfg_lexer.mll
+++ ocaml/xenops-cli/xn_cfg_lexer.mll
@@ -1,11 +1,12 @@
 {
     open Xn_cfg_parser
+	let unquote x = String.sub x 1 (String.length x - 2)
 }
 rule token = parse
 	  ['a'-'z']['_''0'-'9''a'-'z']* as x { IDENT x }
 	| ['0'-'9']['0'-'9''a'-'f''x']* as x { INT (int_of_string x) }
-    | '\''([^'\'''\n']|'.')*'\'' as x { STRING x }
-    | '"'([^'"''\n']|'.')*'"' as x { STRING x }
+    | '\''([^'\'''\n']|'.')*'\'' as x { STRING (unquote x) }
+    | '"'([^'"''\n']|'.')*'"' as x { STRING (unquote x) }
 	| ','                  { COMMA }
 	| '['                  { LBRACKET }
 	| ']'                  { RBRACKET }
