30fda103ba93dceea20c6e93246badf06cbaad12
David Scott <dave.scott@eu.citrix.com>
2013-02-02 14:13:46 +0000

    rrdd: the exported rrdd interface shouldn't include types from the Xenctrl module
    Otherwise generic uses of rrdd end up depending on xen, for no reason.

--- ocaml/rrdd/interface/monitor_types.ml
+++ ocaml/rrdd/interface/monitor_types.ml
@@ -15,7 +15,6 @@
  * @group Performance Monitoring
  *)
 
-(*
 type vcpuinfo = {
 	online: bool;
 	blocked: bool;
@@ -23,6 +22,8 @@ type vcpuinfo = {
 	cputime: int64;
 	cpumap: int32;
 }
+
+(*
 
 let from_xenctrl_vcpuinfo vi : Xenctrl.vcpuinfo -> vcpuinfo = {
 	online = vi.Xenctrl.online;
@@ -35,7 +36,6 @@ let from_xenctrl_vcpuinfo vi : Xenctrl.vcpuinfo -> vcpuinfo = {
 
 open Fun
 open Stringext
-open Xenctrl
 
 type vcpu = {
 	vcpu_sumcpus: float;
@@ -108,20 +108,6 @@ module Vif_device = struct
 		devid: int;
 	}
 end
-
-let vif_device_of_string x =
-	let open Vif_device in
-	try
-		let ty = String.sub x 0 3 and params = String.sub_to_end x 3 in
-		let domid, devid = Scanf.sscanf params "%d.%d" (fun x y -> x,y) in
-		let di = Xenctrl.with_intf (fun xc -> Xenctrl.domain_getinfo xc domid) in
-		let uuid = Uuid.uuid_of_int_array di.Xenctrl.handle |> Uuid.to_string in
-		let vif = (uuid, string_of_int devid) in
-		match ty with
-		| "vif" -> Some { pv = true; vif = vif; domid = domid; devid = devid }
-		| "tap" -> Some { pv = false; vif = vif; domid = domid; devid = devid }
-		| _ -> failwith "bad device"
-	with _ -> None
 
 let string_of_vif_device x =
 	let open Vif_device in
