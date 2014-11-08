f00119660ae12dbf68618ff783d7da3bc14535c4
David Scott <dave.scott@eu.citrix.com>
2014-11-08 22:12:06 +0000

    Use the external (irmin) db rather than than the local one
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/database/db_cache.ml
+++ ocaml/database/db_cache.ml
@@ -14,13 +14,58 @@
 
 
 open Db_interface
+open Threadext
 
 module D=Debug.Make(struct let name="db_cache" end)
 open D
 
+let path = ref "/tmp/foo"
+
+let cached_fd = ref None
+let get_connection =
+        fun () ->
+                match !cached_fd with
+                | Some fd -> fd
+                | None ->
+                        let x = Unixext.open_connection_unix_fd !path in
+                        cached_fd := Some x;
+                        x
+
+let rpc url content_type request_txt =
+        let version = "1.1" in
+        let content_length = String.length request_txt in
+        let request = Http.Request.make ~version
+                ~user_agent:"database_test"
+                ~body:request_txt
+                ~length:(Int64.of_int content_length) Http.Post url in
+        let open Xmlrpc_client in
+        let fd = get_connection () in
+        try
+                Some (with_http request
+                        (fun (response, fd) ->
+                                match response.Http.Response.content_length with
+                                        | None -> failwith "Need a content-length"
+                                        | Some l -> Db_interface.String
+                                                   (Unixext.really_read_string fd (Int64.to_int l))
+                        ) fd)
+        with e ->
+                cached_fd := None;
+                Unix.close fd;
+                None
+
+let m = Mutex.create ()
+let rec rpc_common url content_type request_txt =
+        match (Mutex.execute m (fun () -> rpc url content_type request_txt)) with
+        | None -> rpc_common url content_type request_txt
+        | Some x -> x
+
+module Irmin_db = Db_rpc_client_v1.Make(struct
+        let initialise () = ()
+        let rpc request = rpc_common "/post_remote_db_access" "text/xml" request
+end)
 
 (** Masters will use this to modify the in-memory cache directly *)
-module Local_db : DB_ACCESS = Db_cache_impl
+module Memory_db : DB_ACCESS = Db_cache_impl
 
 (** Slaves will use this to call the master by XMLRPC *)
 module Remote_db : DB_ACCESS = Db_rpc_client_v1.Make(struct
@@ -29,6 +74,8 @@ module Remote_db : DB_ACCESS = Db_rpc_client_v1.Make(struct
 		ignore (Master_connection.open_secure_connection())
 	let rpc request = Master_connection.execute_remote_fn request Constants.remote_db_access_uri
 end)
+
+module Local_db = Irmin_db
 
 let get = function
 	| Db_ref.In_memory _ -> (module Local_db  : DB_ACCESS)
