69065860ac81e18de93483fdc4f04ff322ad4d9d
David Scott <dave.scott@eu.citrix.com>
2014-11-16 14:23:14 +0000

    Database: test that field modified counters increase on update
    This test currently passes.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/database/db_cache_test.ml
+++ ocaml/database/db_cache_test.ml
@@ -15,6 +15,8 @@
 open Pervasiveext
 open Db_cache_types
 
+open OUnit
+
 let create_test_db () =
   let schema = Test_schemas.many_to_many in
   let db = 
@@ -22,8 +24,27 @@ let create_test_db () =
      ++ (Db_backend.blow_away_non_persistent_fields schema)
      ++ (Db_upgrade.generic_database_upgrade))
       (Database.make schema) in
-
   db
+
+let make_row time = List.fold_left (fun acc (k, v) -> Row.add time k v acc) Row.empty
+
+let find tblname objref db =
+  Table.find "VBD:1" (snd (TableSet.find "VBD" (Database.tableset db)))
+
+let test_field_generation_count () =
+  let db = create_test_db () in
+  (* check that updating a field increases the modified time *)
+  let db =
+      Database.make Test_schemas.schema
+   |> Db_upgrade.generic_database_upgrade
+   |> Db_backend.blow_away_non_persistent_fields Test_schemas.schema
+   |> add_row "VBD" "VBD:1" (make_row 0L [ Db_names.ref, Schema.Value.String "VBD:1"; Db_names.uuid, Schema.Value.String "uuid"; "VM", Schema.Value.String "theVM" ]) in
+  let stats, vbd_1 = find "VBD" "VBD:1" db in
+  let db' = set_field "VBD" "VBD:1" "uuid" (Schema.Value.String "another uuid") db in
+  let stats', vbd_1' = find "VBD" "VBD:1" db' in
+  assert_equal ~printer:Int64.to_string stats.Stat.created stats'.Stat.created;
+  assert_equal ~printer:Int64.to_string stats.Stat.deleted stats'.Stat.deleted;
+  assert_equal ~printer:Int64.to_string (Int64.add stats.Stat.modified 1L) stats'.Stat.modified
 
 let check_many_to_many () = 
   let db = create_test_db () in
@@ -65,11 +86,11 @@ let check_many_to_many () =
   then failwith (Printf.sprintf "check_many_to_many: foo(foo:1).foos expected () got %s" (Sexplib.Sexp.to_string (Schema.Value.sexp_of_t foo_bars)));
   ()
 
-open OUnit
 
 let _ =
   let suite = "db_cache" >:::
               [
+                "field generation counts increase" >:: test_field_generation_count;
                 "many to many" >:: check_many_to_many;
               ] in
   OUnit2.run_test_tt_main (OUnit.ounit2_of_ounit1 suite)
